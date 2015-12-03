c4c2c4f0869537399d593743b1776e8df5c95cc5
Till Rohrmann <trohrmann@apache.org>
2015-12-03 19:40:14 +0100

    Split project into 3 submodules. Common, query window and state query.

---
+++ query-window/src/main/java/com/dataartisans/querywindow/WindowJob.java
@@ -0,0 +1,108 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dataartisans.querywindow;
+
+import com.dataartisans.querycommon.RegistrationService;
+import com.dataartisans.querycommon.zookeeper.ZooKeeperConfiguration;
+import com.dataartisans.querycommon.zookeeper.ZooKeeperRegistrationService;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.typeutils.TupleTypeInfo;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+
+import java.util.Properties;
+
+public class WindowJob {
+	public static void main(String[] args) throws Exception {
+
+		ParameterTool params = ParameterTool.fromArgs(args);
+
+		String zookeeper = params.get("zookeeper", "localhost:2181");
+		String zooKeeperPath = params.get("zkPath", "/akkaQuery");
+		String brokers = params.get("brokers", "localhost:9092");
+		String sourceTopic = params.getRequired("source");
+		String sinkTopic = params.getRequired("sink");
+		Long windowSize = params.getLong("window-size", 10_000);
+		Long checkpointInterval = params.getLong("checkpoint", 1000);
+		String statePath = params.getRequired("state-path");
+
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+		env.enableCheckpointing(checkpointInterval);
+		env.setStateBackend(new FsStateBackend(statePath));
+
+		Properties props = new Properties();
+		props.setProperty("zookeeper.connect", zookeeper);
+		props.setProperty("bootstrap.servers", brokers);
+		props.setProperty("group.id", "window-query-example");
+		props.setProperty("auto.commit.enable", "false");
+		props.setProperty("auto.offset.reset", "largest");
+
+		ZooKeeperConfiguration zooKeeperConfiguration = new ZooKeeperConfiguration(zooKeeperPath, zookeeper);
+
+		RegistrationService registrationService = new ZooKeeperRegistrationService(zooKeeperConfiguration);
+
+		DataStream<Long> inputStream = env
+				.addSource(new FlinkKafkaConsumer<>(sourceTopic,
+						new SimpleLongSchema(),
+						props,
+						FlinkKafkaConsumer.OffsetStore.FLINK_ZOOKEEPER,
+						FlinkKafkaConsumer.FetcherType.LEGACY_LOW_LEVEL));
+
+
+		KeyedStream<Tuple2<Long, Long>, Long> withOne = inputStream.map(new MapFunction<Long, Tuple2<Long, Long>>() {
+			@Override
+			public Tuple2<Long, Long> map(Long value) throws Exception {
+					return Tuple2.of(value, 1L);
+			}
+		})
+				.keyBy(new KeySelector<Tuple2<Long, Long>, Long>() {
+					@Override
+					public Long getKey(Tuple2<Long, Long> value) throws Exception {
+						return value.f0;
+					}
+				});
+
+		TupleTypeInfo<Tuple2<Long, Long>> resultType =
+				new TupleTypeInfo<>(BasicTypeInfo.LONG_TYPE_INFO, BasicTypeInfo.LONG_TYPE_INFO);
+
+
+		DataStream<Tuple2<Long, Long>> result = withOne
+				.transform("Query Window",
+						resultType,
+						new QueryableWindowOperator(windowSize, registrationService));
+
+		result.addSink(
+				new FlinkKafkaProducer<>(
+						brokers,
+						sinkTopic,
+						new SimpleStringSerializationSchema<Tuple2<Long, Long>>()))
+				.disableChaining();
+
+		env.execute("Query Window Example");
+	}
+
+}
---
+++ query-window/src/test/java/com/dataartisans/querywindow/TestQueryableWindowOperator.java
@@ -0,0 +1,155 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dataartisans.querywindow;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.pattern.Patterns;
+import akka.testkit.JavaTestKit;
+import akka.util.Timeout;
+import com.dataartisans.querycommon.RegistrationService;
+import com.dataartisans.querycommon.RetrievalService;
+import com.dataartisans.querycommon.actors.QueryActor;
+import com.dataartisans.querycommon.messages.QueryState;
+import com.dataartisans.querycommon.zookeeper.ZooKeeperConfiguration;
+import com.dataartisans.querycommon.zookeeper.ZooKeeperRegistrationService;
+import com.dataartisans.querycommon.zookeeper.ZooKeeperRetrievalService;
+import com.typesafe.config.Config;
+import org.apache.curator.test.TestingServer;
+import org.apache.flink.configuration.ConfigConstants;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.runtime.StreamingMode;
+import org.apache.flink.runtime.akka.AkkaUtils;
+import org.apache.flink.runtime.akka.ListeningBehaviour;
+import org.apache.flink.runtime.instance.ActorGateway;
+import org.apache.flink.runtime.jobgraph.JobGraph;
+import org.apache.flink.runtime.jobgraph.JobStatus;
+import org.apache.flink.runtime.messages.JobManagerMessages;
+import org.apache.flink.runtime.testutils.JobManagerActorTestUtils;
+import org.apache.flink.test.util.ForkableFlinkMiniCluster;
+import org.apache.flink.test.util.TestBaseUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import scala.Option;
+import scala.Some;
+import scala.Tuple2;
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+import scala.concurrent.duration.FiniteDuration;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class TestQueryableWindowOperator {
+
+	private static ActorSystem actorSystem;
+	private static TestingServer zkServer;
+	private static ForkableFlinkMiniCluster cluster;
+	private static Configuration config = new Configuration();
+
+	private static int numberTaskManager = 2;
+	private static int numberSlots = 2;
+	private static int parallelism = numberTaskManager * numberSlots;
+	private static long windowSize = 1000;
+
+	private static int queryAttempts = 10;
+	private static FiniteDuration queryTimeout = new FiniteDuration(1, TimeUnit.SECONDS);
+
+	private static FiniteDuration timeout = new FiniteDuration(20, TimeUnit.SECONDS);
+
+	@BeforeClass
+	public static void setup() throws Exception {
+		Option<Tuple2<String, Object>> remoting = new Some<>(new scala.Tuple2<String, Object>("", 0));
+
+		Config akkaConfig = AkkaUtils.getAkkaConfig(config, remoting);
+		actorSystem = ActorSystem.create("TestingActorSystem", akkaConfig);
+
+		zkServer = new TestingServer(true);
+
+		config.setInteger(ConfigConstants.LOCAL_NUMBER_TASK_MANAGER, numberTaskManager);
+		config.setInteger(ConfigConstants.TASK_MANAGER_NUM_TASK_SLOTS, numberSlots);
+
+		cluster = TestBaseUtils.startCluster(config, StreamingMode.STREAMING, false);
+	}
+
+	@AfterClass
+	public static void teardown() throws Exception {
+		if (actorSystem != null ) {
+			JavaTestKit.shutdownActorSystem(actorSystem);
+			actorSystem = null;
+		}
+
+		if (zkServer != null) {
+			zkServer.close();
+			zkServer = null;
+		}
+
+		if (cluster != null) {
+			TestBaseUtils.stopCluster(cluster, timeout);
+		}
+	}
+
+	@Test
+	public void testQueryableWindowOperator() throws Exception {
+		ActorGateway leader = cluster.getLeaderGateway(timeout);
+
+		ZooKeeperConfiguration zooKeeperConfiguration = new ZooKeeperConfiguration(
+			"/test",
+			zkServer.getConnectString());
+
+		RegistrationService registrationService = new ZooKeeperRegistrationService(zooKeeperConfiguration);
+		RetrievalService<Long> retrievalService = new ZooKeeperRetrievalService<>(zooKeeperConfiguration);
+
+		JobGraph job = TestJob.getTestJob(parallelism, windowSize, registrationService);
+
+		leader.tell(new JobManagerMessages.SubmitJob(job, ListeningBehaviour.DETACHED));
+
+		ActorRef queryActor = actorSystem.actorOf(Props.create(QueryActor.class, retrievalService, queryTimeout, queryAttempts), "QueryActor");
+
+		Random rnd = new Random();
+
+		boolean continueQuery = true;
+		int counter = 0;
+
+		while (continueQuery) {
+			long state = rnd.nextInt(10);
+
+			Future<Object> futureResult = Patterns.ask(queryActor, new QueryState<Long>(state), new Timeout(timeout));
+
+			Object result = Await.result(futureResult, timeout);
+
+			System.out.println(result);
+
+			counter++;
+
+			continueQuery = counter < 10000;
+		}
+
+		leader.tell(new JobManagerMessages.CancelJob(job.getJobID()));
+
+		JobManagerActorTestUtils.waitForJobStatus(
+			job.getJobID(),
+			JobStatus.CANCELED,
+			leader,
+			timeout
+		);
+	}
+}
